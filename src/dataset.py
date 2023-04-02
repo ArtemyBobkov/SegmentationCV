9c126233be09e91ed59ee1163bc3ea6dfe8e95ff
ArtemyBobkov <bobkov.aa@phystech.edu>
2023-04-02 18:48:12 +0300

    Simple project structure made

---
+++ src/dataset.py
@@ -0,0 +1,10 @@
+from torch.utils.data import Dataset
+
+class LSCPlantsDataset(Dataset):
+    def __init__(self, path: str) -> None:
+        
+
+    def __getitem__(self, index: int):
+
+
+    def __len__(self) -> int:
